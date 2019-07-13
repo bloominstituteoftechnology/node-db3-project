b302ce33779fe859e6e343d9dd9e67a5a07b9a83
Diandra Ryan-Mas <dryanmas@Diandras-MacBook-Air.local>
2019-07-13 08:16:26 -0700

    schema setup

---
+++ data/migrations/20190713080440_create-tables.js
@@ -0,0 +1,29 @@
+
+exports.up = function(knex) {
+  return knex.schema
+    .createTable('schemes', tbl => {
+      tbl.increments();
+      tbl.string('scheme_name', 128)
+        .unique()
+        .notNullable();
+    })
+    .createTable('steps', tbl => {
+      tbl.increments();
+      tbl.integer('step_number')
+        .unsigned()
+        .notNullable();
+      tbl.integer('scheme_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('schemes')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE');
+    });
+};
+
+exports.down = function(knex) {
+  return knex.schema
+    .dropTableIfExists('steps')
+    .dropTableIfExists('schemes');
+};
