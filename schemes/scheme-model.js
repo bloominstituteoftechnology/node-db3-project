36068fdf66a19cd4a169074be33019d4fe913426
Orie Chinedu <oriechinedu@gmail.com>
2019-07-17 14:49:32 +0100

    Implement helper to get all schemes

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,2 +1,17 @@
 
 
+const db = require('./config')
+const { ErrorHandler } = require('../helpers')
+
+const find = async () => {
+  try {
+    const schemes = await db('schemes');
+    return schemes
+  } catch (error) {
+    throw new ErrorHandler(500, 'Internal server error')
+  }
+}
+
+module.exports = {
+  find
+}
