0fa39ce330563670d41752e92bc673b486221d27
Orie Chinedu <oriechinedu@gmail.com>
2019-07-17 14:51:45 +0100

    Implement helper to get a scheme by ID

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -2,14 +2,23 @@
 
 const db = require('./config')
 const { ErrorHandler } = require('../helpers')
-
+const msg ='Internal server error'
 const find = async () => {
   try {
     const schemes = await db('schemes');
     return schemes
   } catch (error) {
-    throw new ErrorHandler(500, 'Internal server error')
+    throw new ErrorHandler(500, msg)
   }
+}
+
+const findById = async (id) => {
+try {
+  const scheme = await db('schemes').where({ id })
+  return scheme
+} catch (error) {
+  throw new ErrorHandler(500, msg)
+}
 }
 
 module.exports = {
