30ff593cadf613d5fc8925f4506bafb69c0d205a
Yusuf Nafey <yusufnafey@yahoo.com>
2019-08-14 14:53:36 -0700

    finishes mvp

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -3,7 +3,7 @@ const db = require("../db-config");
 module.exports = {
   find,
   findById,
-  //   findSteps,
+  findSteps,
   add,
   update,
   remove
@@ -15,6 +15,21 @@ function find() {
 
 function findById(id) {
   return db("schemes").where({ id: id });
+}
+
+function findSteps(id) {
+  //   select steps.id, schemes.scheme_name, steps.step_number, steps.instructions
+  //   from steps
+  //   inner join schemes on steps.scheme_id = schemes.id
+  return db("steps")
+    .innerJoin("schemes", "steps.scheme_id", "=", "schemes.id")
+    .select(
+      "steps.id",
+      "schemes.scheme_name",
+      "steps.step_number",
+      "steps.instructions"
+    )
+    .where({ scheme_id: id });
 }
 
 function add(scheme) {
