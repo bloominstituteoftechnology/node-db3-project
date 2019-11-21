cbef45fba9cca36bc6890986ed7bc2a64b56e97b
Brandon Davis <49970067+Bdavis90@users.noreply.github.com>
2019-11-20 19:24:17 -0500

    fixed findById helper function layout and added add and update helper function

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -2,7 +2,10 @@ const db = require("../data/db-config");
 
 module.exports = {
   find,
-  findById
+  findById,
+  findSteps,
+  add,
+  update
 };
 
 function find() {
@@ -11,4 +14,23 @@ function find() {
 
 function findById(id) {
   return db("schemes").where({ id });
+}
+function findSteps(id) {
+  return db("steps as s")
+    .join("schemes as sc", "scheme_id", "sc.id")
+    .select("scheme_name", "step_number", "instructions")
+    .where({ scheme_id: id })
+    .orderBy("step_number");
+}
+
+function add(scheme) {
+  return db("schemes")
+    .insert(scheme)
+    .then(ids => ({ id: ids[0] }));
+}
+
+function update(changes, id) {
+  return db("schemes")
+    .where({ id })
+    .update(changes);
 }
