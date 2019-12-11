44122a6419368e23058b470935a505ccac1473d1
Ale <alearanibar@gmail.com>
2019-12-11 12:36:03 -0800

    start

---
+++ schemes/scheme-model.js
@@ -0,0 +1,24 @@
+const db = require('../data/db-config')
+
+module.exports = {
+    find,
+    findById,
+    findSteps,
+    add,
+    update,
+    remove
+}
+
+function find() {
+    return db('schemes')
+}
+
+function findById(id) {
+    return db('schemes')
+    .where({id})
+    .first()
+}
+
+function findsteps(id) {
+
+}
