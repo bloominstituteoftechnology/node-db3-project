aa3bf7d82ddcdc033534e05b842754ed9da6c41a
Chrismis79 <chrismis79@gmail.com>
2019-12-11 16:55:47 -0600

    finally got 3 sql queries working

--- queries.sql
+++ queries.sql
@@ -7,7 +7,18 @@ join Category as Cat
   on P.CategoryId = Cat.id
 
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
+SELECT O.id, O.ShipVia, O.OrderDate, S.CompanyName, S.id 
+FROM [order] as O
+JOIN Shipper as S
+  on O.ShipVia = S.id
+  WHERE O.OrderDate < '2012-08-09'
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
+SELECT OD.OrderId, OD.Quantity, P.ProductName 
+FROM OrderDetail as OD
+JOIN [Order] as O ON OD.OrderId = O.Id
+Join Product as P ON OD.ProductId = P.id
+WHERE O.ID = 10251
+ORDER BY P.ProductName
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
