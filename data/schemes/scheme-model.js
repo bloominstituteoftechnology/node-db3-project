d0f1b1beddcd6b3d3b8927e67d5226813b4cc7b1
cappers86 <52895127+cappers86@users.noreply.github.com>
2020-01-22 15:31:58 +0000

    Added db config plus  find and find by id

---
+++ data/schemes/scheme-model.js
@@ -0,0 +1,18 @@
+const db = require('../db-config');
+
+module.exports = {
+    find,
+    findById,
+    findSteps,
+    add,
+    update,
+    remove
+};
+
+function find() {
+    return db('schemes');
+}
+
+function findById(id) {
+return db('schemes').where({id});
+}
