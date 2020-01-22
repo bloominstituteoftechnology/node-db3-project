2391dde2f116783565e1728ade87381df0dac57b
cappers86 <52895127+cappers86@users.noreply.github.com>
2020-01-22 16:17:16 +0000

    schema model finished

--- data/schemes/scheme-model.js
+++ data/schemes/scheme-model.js
@@ -16,3 +16,30 @@ function find() {
 function findById(id) {
 return db('schemes').where({id});
 }
+
+function findSteps(id) {
+    return db('steps as s')
+    .join('schemes as sc', 'scheme_id, sc.id')
+    .select('scheme_name', 'step_number', 'instructions')
+    .where({scheme_id: id})
+    .orderBy('step_number');
+}
+
+function add(scheme) {
+    return db('schemes')
+    .insert(scheme)
+    .then(ids => ({id : ids[0]}))
+} 
+
+function update(changes, id) {
+    return db('schemes')
+    .where({id})
+    .update(changes);
+    
+}
+
+function remove(id) {
+    return db('schemes')
+    .where({id})
+    .dek();
+}
