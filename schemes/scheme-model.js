11d9c3a4ca42e4428ab2d67f15c0b900fea8cdcd
Brett Karr <b.karr@polyclip.us>
2020-02-03 21:57:19 -0600

    MVP met, I think?

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,25 +1,56 @@
 const db = require("../utils/schemesDb.js");
 
-exports.find = () => {
-  return db("schemes");
+const dbInput = "schemes";
+
+const find = () => {
+  return db(dbInput);
 };
 
-exports.findById = id => {
-  return db("schemes")
+const findById = id => {
+  console.log("findById:", id);
+  return db(dbInput)
     .where({ id }) //searching by id
     .first(); //first data that matches
 };
 
-exports.add = user => {
-  db("schemes")
-    .insert(user)
-    .then(ids => {
-      return findById(ids[0]);
-    });
+const add = async newInput => {
+  //inserts new data, and returns an ID
+  const [id] = await db(dbInput).insert(newInput);
+  return findById(id);
 };
 
-exports.findSteps = id => {};
+const findSteps = id => {
+  return db("steps")
+    .select("st.id", "s.scheme_name", "st.step_number", "st.instructions")
+    .join("steps as st", "s.id", "st.scheme_id")
+    .where("s.id", "id");
 
-exports.update = (changes, id) => {};
+  /*
+  return db("steps")
+    .where({ scheme_id: id })
+    .select("step_number", "instructions", "scheme_id", "id");
+*/
+};
 
-exports.remove = id => {};
+const update = async (changes, id) => {
+  console.log("update", id, changes);
+  await db(dbInput)
+    .where({ id })
+    .update(changes, "*");
+  return findById(id);
+};
+
+const remove = id => {
+  return db(dbInput)
+    .where({ id })
+    .del();
+};
+
+module.exports = {
+  remove,
+  update,
+  findSteps,
+  add,
+  findById,
+  find
+};
