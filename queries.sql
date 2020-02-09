bb1257bb2daf37eaa73059545a589dcf3452dfd5
grizzlybrodams <jddaley328@gmail.com$4josh@josh-pcgit config --global user.namegit config --global user.name grizzlybrodams>
2020-02-08 19:18:18 -0700

    finished mvp

--- queries.sql
+++ queries.sql
@@ -1,9 +1,37 @@
 -- Multi-Table Query Practice
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
+select ProductName, CategoryName
+from Product
+join Category
+on Product.CategoryId = Category.Id
 
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
 
+select CompanyName, [Order].Id 
+From [Order]
+join Shipper
+on Shipper.Id = [Order].ShipVia
+Where [Order].OrderDate < '2012-08-09'
+
+
+
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
+select ProductName, Quantity
+From Product
+join OrderDetail
+on Product.Id = OrderDetail.ProductId
+Where OrderDetail.OrderId = '10251'
+
+
+
+
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+
+select CompanyName, LastName as EmployeesLastName, [Order].Id
+From [Order]
+join Employee
+on Employee.Id = [Order].EmployeeId
+join Customer
+on Customer.Id = [Order].CustomerId
---
+++ schemes/scheme-model.js
@@ -0,0 +1,54 @@
+const db = require('../data/dbConfig.js');
+
+
+function find(){
+    return db('schemes')
+}
+
+
+
+function findById(id){
+    return db('schemes')
+    .where({id}) 
+    
+}
+
+
+function findSteps (id){
+    return db('schemes')
+    .join('steps','schemes.id', 'steps.scheme_id' )
+    .select('steps.id', 'schemes.scheme_name', 'steps.step_number', 'steps.instructions' )
+    .where('schemes.id', id)
+    .orderBy('steps.step_number')
+}
+
+
+function add(scheme){
+    return db('schemes')
+    .insert(scheme, )
+}
+
+
+function update(changes,id){
+    return db('schemes')
+    .update(changes)
+    .where({id})
+    
+}
+
+
+
+function remove(id) {
+    return db('schemes')
+    .del()
+    .where({id})
+}
+
+module.exports = {
+    find,
+    findById,
+    findSteps,
+    add,
+    update,
+    remove
+}
