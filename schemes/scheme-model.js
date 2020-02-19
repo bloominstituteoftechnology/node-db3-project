de5f834f0fbae79ef46960ceba8666ae38d4404a
Jonathan Hanson <jhanson8@uoregon.edu>
2020-02-19 15:57:28 -0800

    scheme-model.js

---
+++ schemes/scheme-model.js
@@ -0,0 +1,44 @@
+/* jshint esversion: 6 */
+const db = require("../data/db-config.js.js");
+
+module.exports = {
+  find,
+  findById,
+  findSteps,
+  add,
+  update,
+  remove
+};
+
+function find() {
+  return db("schemes");
+}
+
+function findById(id) {
+  return db("schemes")
+    .where({ id })
+    .first();
+}
+
+function findSteps(id) {
+  return db("schemes")
+    .join("schemes", "schemes.id", "steps.scheme_id")
+    .select("schemes.scheme_name", "steps.step_number", "steps.instructions")
+    .where("scheme_name", name);
+}
+
+function add(scheme) {
+  return db("schemes").insert(scheme, "id");
+}
+
+function update(id, changes) {
+  return db("schemes")
+    .where({ id })
+    .update(changes);
+}
+
+function remove(id) {
+  return db("schemes")
+    .where({ id })
+    .del();
+}
