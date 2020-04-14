7e411d843b3253e6efc08e7191b4a144229d47bb
Ana Tulea <anatulea@MacBook-Pro-de-Ana.local>
2020-04-14 13:24:29 -0700

    Completed the query practice

--- queries.sql
+++ queries.sql
@@ -1,9 +1,33 @@
 -- Multi-Table Query Practice
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
+select ProductName, CategoryName 
+from Product
+join Category
+on CategoryId = Category.id;
+-- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. 
+--Shows 429 records.
+select [order].id,[order].OrderDate, CompanyName 
+from [order]
+join Shipper
+on ShipVia = Shipper.id
+where [order].orderDate < date('2012-08-09');
 
--- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
+-- Display the name and quantity of the products ordered in order with Id 10251. 
+--Sort by ProductName. Shows 3 records.
+select p.ProductName, o.Quantity
+from [OrderDetail] as o
+join [Product] as p
+ on p.id = o.ProductId
+where o.OrderId = 10251
+order by p.ProductName;
 
--- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
-
--- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+-- Display the OrderID, Customer's Company Name and the employee's LastName for every order.
+-- All columns should be labeled clearly. Displays 16,789 records.
+select c.CompanyName, o.ID, e.LastName
+from [Order] as o
+join [Customer] as c
+on c.Id = o.CustomerId
+join [Employee] as e
+on o.EmployeeId= e.Id 
+order by e.LastName;
