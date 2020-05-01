5752f38418f61e1bd24e65c789ce7b3774802b6b
Michael Lowe <mrlowememphis@yahoo.com>
2020-04-30 19:48:23 -0500

    dbConfig created

--- queries.sql
+++ queries.sql
@@ -2,6 +2,8 @@
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
 
+
+
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
