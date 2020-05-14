310066d89ee0ac10c44e21a1246c8914fea689eb
Golden Eagle <joowoon.kang@biola.edu>
2020-05-13 18:05:56 -0700

    finished everything except remove helper

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -3,38 +3,96 @@ const db = require("../data/db-config.js");
 module.exports = {
   find,
   findById,
+  findSteps,
   add,
+  addStep,
   update,
   remove,
 };
 
 function find() {
-  return db("scheme");
+  return db("schemes");
 }
 
 function findById(id) {
-  return db("scheme").where("id", id).first();
+  return db("schemes").where("id", id).first();
 }
 
-function add(user) {
-  return db("scheme")
-    .insert(user, "id")
+function findSteps(id) {
+  return db("steps as st")
+    .where("scheme_id", id)
+    .join("schemes as sc", "st.scheme_id", "=", "sc.id")
+    .select("st.id", "sc.scheme_name", "st.step_number", "st.instructions")
+    .orderBy("st.step_number");
+}
+// function all() {
+//     /*
+//       select p.contents as quote, u.username as saidBy
+//       from posts as p
+//       join users as u on p.user_id = u.id;
+//     */
+
+//     return db("posts as p")
+//       .join("users as u", "p.user_id", "=", "u.id")
+//       .select("p.contents as quote", "u.username as saidBy");
+//   }
+
+//   function findById(id) {
+//     return db("posts").where("id", id).first();
+//   }
+
+//   function add(post) {
+//     return db("posts")
+//       .insert(post, "id")
+//       .then(ids => {
+//         return findById(ids[0]);
+//       });
+//   }
+
+//   /*
+
+function add(scheme) {
+  return db("schemes")
+    .insert(scheme, "id")
+    .then((ids) => {
+      return findById(ids[0]);
+    });
+}
+
+function addStep(stepData, id) {
+  return db("steps")
+    .insert(stepData, "id")
     .then((ids) => {
       return findById(ids[0]);
     });
 }
 
 /*
-db('scheme') => a promise that resolves to a user
+db('schemes') => a promise that resolves to a user
 findById  => a promise that resolves to a user
 add  => a promise that resolves to a user
 post
 */
 
-function update(id, changes) {
-  return db("scheme").where({ id }).update(changes);
+function update(changes, id) {
+  return db("schemes as sc")
+    .where({ id }) //{id} grabbing whole object
+    .update(changes)
+    .then(() => {
+      return findById(id);
+    });
 }
+// return db("steps as st")
+// .where("scheme_id", id)
+// .join("schemes as sc", "st.scheme_id", "=", "sc.id")
+// .select("st.id", "sc.scheme_name", "st.step_number", "st.instructions")
+// .orderBy("st.step_number");
+// }
 
 function remove(id) {
-  return db("scheme").where({ id }).del();
+  console.log({ id });
+  return db("schemes as sc")
+    .where({ id })
+    .select("sc.id", "sc.scheme_name")
+    .del();
 }
