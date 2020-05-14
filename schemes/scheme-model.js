7e2c0baa72520b64671e6aa1b477342763f46291
Lisa Maskovich <lisamariejensenmaskovich@gmail.com>
2020-05-13 17:15:57 -0700

    Coming along, but I am having troubles with some CRUD opps on Postman

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,3 +1,56 @@
+const db = require('../data/dbConfig');
+
+function find() {
+  return db('schemes');
+}
+
+function findById(id) {
+  return db('schemes')
+  .where({id})
+  .first();
+}
+
+function findSteps(scheme_id) {
+  return db("steps")
+  .join("schemes", "schemes.id", "steps.scheme_id")
+  .select("steps.instructions", "steps.step_number")
+  .where({scheme_id})
+}
+
+// function findSteps(id) {
+//   return db("schemes")
+//   .join("steps", "schemes.id", "steps.scheme_id")
+//   .select("steps.id")
+//   .where("schemes.id", id)
+//   .orderBy("steps.step_number");
+// }
+
+
+function add(scheme) {
+  return db("schemes")
+  .insert(scheme)
+  .then((id) => {
+    return findById(id[0]);
+  });
+}
+
+// function add(scheme) {
+//   return db("schemes")
+//   .insert(scheme, );
+// }
+
+function update(changes, id) {
+  return db("schemes")
+  .update(changes)
+  .where({id});
+}
+
+function remove(id) {
+  return db("schemes")
+  .del()
+  .where({id});
+}
+
 module.exports = {
   find,
   findById,
@@ -5,5 +58,9 @@ module.exports = {
   add,
   update,
   remove
-}
+};
+
+
+
+
 
