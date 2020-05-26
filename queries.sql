f48d282f7e4cee839306be52051a6c27ce101bf9
Abdulhamid Azizy <55733008+HamidAzizy@users.noreply.github.com>
2020-05-25 17:28:14 -0700

    Project Completed

--- queries.sql
+++ queries.sql
@@ -2,8 +2,31 @@
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
 
+SELECT ProductName, CategoryName
+FROM product AS P
+    JOIN Category AS C
+    ON P.CategoryId = C.id;
+
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
+
+select [order].id, orderdate, companyname
+from [order]
+    join shipper
+    on [order].shipvia = shipper.id
+where [order].orderdate < '2012-08-09'
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
+select ProductName, Quantity
+from Product
+    join OrderDetail
+    on product.Id = orderdetail.ProductId
+where orderdetail.OrderId = '10251'
+order by product.ProductName
+
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+
+select distinct orderid, companyname, lastname
+from orderdetail
+    join [order] , employee, customer
+on orderdetail.orderid = [order].id and [order].EmployeeId = employee.id and [order].customerid = customer.id
---
+++ schemes/scheme-model.js
@@ -0,0 +1,45 @@
+const knex = require('knex')
+const config = require('../knexfile.js')
+const db = knex(config.development)
+
+module.exports = {
+    find,
+    findById,
+    findSteps,
+    add,
+    update,
+    remove,
+};
+
+function find() {
+    return db('schemes');
+}
+
+function findById(id) {
+    return db('schemes').where({ id }).first();
+}
+
+function findSteps(id) {
+    return db('schemes')
+        .join('steps')
+        .select('schemes.id as schemesId', 'schemes.scheme_name', 'steps.step_number', 'steps.step_number', 'steps.instructions')
+        .where({ schemesId: id });
+}
+
+function add(scheme) {
+    return db('schemes')
+        .insert(scheme)
+}
+
+function update(changes, id) {
+    console.log('update')
+    return db('schemes')
+        .where({ id })
+        .update(changes)
+}
+
+function remove(id) {
+    return db('schemes')
+        .where('id', id)
+        .del();
+}
