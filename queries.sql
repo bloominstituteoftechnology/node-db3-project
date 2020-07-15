62223376a20615259add1fc17e326e7244ff66d0
Lambda_School_Loaner_277 <lambda_school_loaner_277@Lambda-School-Loaner-277s-MacBook-Pro.local>
2020-07-15 19:02:30 -0400

    Complete and Working

--- queries.sql
+++ queries.sql
@@ -1,9 +1,29 @@
 -- Multi-Table Query Practice
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
+SELECT P.ProductName,
+C.CategoryName FROM Product AS P
+JOIN [Category] AS C ON P.CategoryId = C.Id;
 
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
+SELECT O.Id, S.CompanyName
+FROM [Order] AS O
+JOIN [Shipper] AS S
+ON O.ShipVia = S.Id
+WHERE O.OrderDate < "2012-08-09";
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
+SELECT P.ProductName, O.Quantity
+FROM [OrderDetail] AS O
+JOIN [Product] AS P
+ON O.ProductId = P.Id
+WHERE O.OrderId = 10251
+ORDER BY P.ProductName
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+SELECT DISTINCT O.Id AS 'Order id', C.CompanyName AS 'Company Name',  E.LastName AS 'Employee Last Name'
+FROM [Order] AS O
+JOIN [Customer] AS C
+JOIN [Employee] AS E
+ON O.CustomerId = C.Id
+WHERE O.EmployeeId = E.Id;
---
+++ schemes/scheme-model.js
@@ -0,0 +1,60 @@
+const db = require("../data/dbConfig");
+
+module.exports = {
+  find,
+  findById,
+  findSteps,
+  add,
+  update,
+  remove,
+};
+
+function find() {
+  return db("schemes");
+}
+
+function findById(id) {
+  return db("schemes").where("id", id).first();
+}
+
+function findSteps(id) {
+  return db("steps")
+    .where("steps.scheme_id", id)
+    .join("schemes", "steps.scheme_id", "schemes.id")
+    .select(
+      "steps.id",
+      "schemes.scheme_name",
+      "steps.step_number",
+      "steps.instructions"
+    );
+}
+
+function add(scheme) {
+  return db("schemes")
+    .insert(scheme)
+    .then((scheme) => {
+      return findById(scheme[0]);
+    });
+}
+
+function update(changes, id) {
+  return db("schemes")
+    .where("id", id)
+    .first()
+    .update(changes)
+    .then(() => {
+      findById(id);
+    });
+}
+
+function remove(id) {
+  return findById(id).then((scheme) => {
+    return db("schemes")
+      .where({ id })
+      .first()
+      .del()
+      .then((scheme) => {
+        return scheme;
+      });
+  });
+}
