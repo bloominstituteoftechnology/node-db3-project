7de2ed37439f45453d18ebe13602d40d5b7efc26
lex-marie790 <lexi.marie790@gmail.com>
2020-07-28 20:54:29 -0700

    module layout setup, db-config added

---
+++ schemes/scheme-model.js
@@ -0,0 +1,29 @@
+const db = require('../data/db-config.js');
+
+function find() {
+
+}
+
+function findById(id) {
+    
+}
+
+function findSteps(id) {
+    
+}
+
+function add(schemeData) {
+    
+}
+
+function remove(id) {
+    
+}
+
+module.exports = {
+    find,
+    findById,
+    findSteps,
+    add,
+    remove
+}
