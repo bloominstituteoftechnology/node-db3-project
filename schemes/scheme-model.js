5c3ce06e7980dbe1a71aaf4c46dab926ad9e2a4a
Nic <53277092+npm-nic@users.noreply.github.com>
2020-08-12 16:25:28 -0500

    ⚔️ MVP: database methods include add + update scheme

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -4,9 +4,9 @@ module.exports = {
   find,
   findById,
   findSteps,
-  // add,
-  // addStep
-  // update
+  add,
+  // addStep, // (stretch)
+  update,
   remove,
 };
 
@@ -17,6 +17,7 @@ function find() {
 
 // // --> return specific scheme using scheme id <-- // //
 function findById(id) {
+  // console.log("findById -> id", id);
   //  [a]
   return db("schemes").where({ id }).first();
 }
@@ -26,14 +27,34 @@ function findSteps(id) {
   return db("steps").where({ scheme_id: id });
 }
 
+// // --> add a new scheme + return scheme object w/id <-- // //
+function add(scheme) {
+  return db("schemes")
+    .insert(scheme)
+    .then((ids) => {
+      const id = ids[0];
+      return findById(id);
+    });
+}
+
+// // --> add new step to scheme + return <-- // //
+// (STRETCH)
+
+// // --> update scheme + return updated scheme object  <-- // //
+function update(changes, id) {
+  return db("schemes")
+    .where({ id })
+    .update(changes)
+    .then(() => {
+      return findById(id);
+    });
+}
+
 // // --> remove scheme using scheme id <-- // //
 function remove(id) {
-  //  [b]
   return db("schemes").where({ id }).del();
 }
 
 //  [a] : {id} is the same as {id: id} which means id === id in SQL speak
 //  first()
 //  --> an array is always returned whether its empty or not
-
-//  [b] : del() does the magic
