cc6b5b83713bb0c23ab0df91a1fcb1e7d2b59bf3
Darrell-Conklin <dmconklin84@gmail.com>
2020-08-25 18:21:44 -0500

    added database methods to the model file

---
+++ schemes/scheme-model.js
@@ -0,0 +1,45 @@
+const db = require('../data/config')
+
+function find() {
+    return db('schemes')
+        .select('scheme_name')
+}
+
+function findById(id) {
+    return db('schemes')
+        .select('scheme_name')
+        .where('id', id)
+}
+
+function findSteps(id) {
+    return db('schemes as sc')
+        .innerJoin('steps as st', 'sc.id', 'st.scheme_id')
+        .where('sc.id', id)
+        .select('sc.id', 'sc.scheme_name', 'st.step_number', 'st.instructions')
+}
+
+function add(scheme) {
+    return db('schemes')
+        .insert(scheme)
+}
+
+function addStep(stepData, id) {
+    return db('steps')
+        .insert({...stepData, scheme_id: id})
+}
+
+function update(changes, id) {
+    return db('schemes')
+        .update(changes)
+        .where('id', id)
+}
+
+function remove(id) {
+    db('schemes')
+        .where('id', id)
+        .del()
+}
+
+module.exports = {
+    find, findById, findSteps, add, addStep, update, remove
+}
