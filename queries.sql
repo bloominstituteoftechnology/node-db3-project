8bd0cdf72113231e882b0c4aa9dca12b8ef0255d
Evelyn Nguyen <Evelyn@viets-imacx.lan>
2020-08-27 15:26:41 -0700

    Q&A

--- queries.sql
+++ queries.sql
@@ -1,9 +1,34 @@
 -- Multi-Table Query Practice
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
-
+        -- SELECT "ProductName", "CategoryName"
+        -- FROM "Product" 
+        -- JOIN "Category"
+        -- ON Product.CategoryId = Category.id;
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
-
+        -- SELECT O.Id, S.CompanyName
+        -- FROM "Order" AS O
+        -- JOIN "Shipper" AS S
+        -- ON O.ShipVia = S.Id
+        -- WHERE OrderDate < date('2012-08-09');
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
-
+        -- SELECT O.Id, S.CompanyName
+        -- FROM "Order" AS O
+        -- JOIN "Shipper" AS S
+        -- ON O.ShipVia = S.Id
+        -- WHERE OrderDate < date('2012-08-09');
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+        -- SELECT 
+	-- OD.OrderId,
+ 	-- C.CompanyName, 
+ 	-- E.LastName "Employee Last Name"
+        -- FROM 
+        --         "OrderDetail" AS OD
+        -- JOIN 
+        --         "ORDER" AS O ON O.Id = OD.OrderId
+        -- JOIN	
+        --         "Employee" AS E ON O.EmployeeId = E.Id
+        -- JOIN 
+        --         "Customer" AS C ON O.CustomerId = C.Id 
+
+        -- LIMIT 30;
