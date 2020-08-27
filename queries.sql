61623ff232d60b761b00d98d3a9e9a2a85bf8c9f
Amy Davis <amydavis@Blakes-MacBook-Pro-2.local>
2020-08-27 19:05:56 -0400

    Creating queries

--- queries.sql
+++ queries.sql
@@ -2,7 +2,15 @@
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
 
+SELECT "product"."ProductName", "Category"."CategoryName"
+FROM "Product"
+JOIN "Category" ON "Category"."Id" = "Product"."CategoryId";
+
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
+SELECT "Order"."Id" AS "OrderID", "Shipper"."CompanyName", "Order"."OrderDate" AS "OrderDate"
+FROM "Order"
+JOIN "Shipper" ON "Order"."ShipVia" = "Shipper"."Id"
+WHERE "OrderDate" < "2012-08-09"
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
