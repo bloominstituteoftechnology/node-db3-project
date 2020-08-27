8b502fd6933fe1078e4f8d13f11844db1e9e13be
Amy Davis <amydavis@Blakes-MacBook-Pro-2.local>
2020-08-27 19:49:58 -0400

    created scheme model, updated syntax in queries

--- queries.sql
+++ queries.sql
@@ -10,16 +10,16 @@ JOIN "Category" ON "Category"."Id" = "Product"."CategoryId";
 SELECT "Order"."Id" AS "OrderID", "Shipper"."CompanyName", "Order"."OrderDate" AS "OrderDate"
 FROM "Order"
 JOIN "Shipper" ON "Order"."ShipVia" = "Shipper"."Id"
-WHERE "OrderDate" < "2012-08-09"
+WHERE "OrderDate" < "2012-08-09";
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 SELECT "OrderDetail"."OrderId", "Product"."ProductName", "OrderDetail"."Quantity"
 FROM "OrderDetail"
 JOIN "Product" ON "OrderDetail"."ProductId" = "Product"."Id"
-WHERE "OrderId" = 10251
+WHERE "OrderId" = 10251;
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
 SELECT "Order"."Id" AS "OrderId", "Customer"."CompanyName", "Employee"."LastName" AS "EmployeeLastName"
 FROM "Order"
 JOIN "Customer" ON "Order"."CustomerId" = "Customer"."Id"
-LEFT JOIN "Employee" ON "Order"."EmployeeId" = "Employee"."LastName";
+JOIN "Employee" ON "Order"."EmployeeId" = "Employee"."LastName";
