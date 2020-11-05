2dc77dc6426d8c7d3564c8ae4e751fa06cc99409
pvahanian <51976194+pvahanian@users.noreply.github.com>
2020-11-05 00:24:52 -0600

    mvp

--- queries.sql
+++ queries.sql
@@ -1,9 +1,39 @@
 -- Multi-Table Query Practice
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
-
+select 
+    p.productname as 'Product Name', 
+    c.categoryname as 'Category Name'
+from Product as p
+join Category as c
+    on p.categoryid = c.id
+    
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
 
+select 
+    o.id as 'OrderId',
+    s.companyName as 'Company Name'
+from "Order" as o
+join Shipper as s
+ on o.shipvia = s.id
+where orderDate < "2012-08-09"
+
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
+select
+    p.Productname as 'name',
+    od.quantity as 'Quantity'
+from orderdetail as od
+join product as p
+    on p.id = od.productid
+where od.orderid = 10251
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+select 
+    o.id as 'OrderID',
+    c.companyname as 'Customer', 
+    e.lastname as 'Employee Last Name'
+from "Order" as o 
+join Customer as c 
+    on o.customerid = c.id
+join Employee as e 
+    on o.employeeid = e.id
--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,1 +1,75 @@
 // scheme-model
+const db = require("../data/db-config.js");
+
+module.exports = {
+//   Calling find returns a promise that resolves to an array of 
+//   all schemes in the database.
+//   No steps are included.
+
+    find() {
+        return db('schemes')
+    },
+
+    // findById(id):
+    // Expects a scheme id as its only parameter.
+    // Resolve to a single scheme object.
+    // On an invalid id, resolves to null, perhaps by doing if (!schemaObject) return Promise.resolve(null).
+    
+    findById(id) {
+        return db('schemes')
+        .where({ id : id }).first();
+    },
+
+    // findSteps(id):
+    // Expects a scheme id.
+    // Resolves to an array of all correctly ordered step for the given scheme:
+    //[ { id: 17, scheme_name: 'Find the Holy Grail', step_number: 1, instructions: 'quest'},
+    // { id: 18, scheme_name: 'Find the Holy Grail', step_number: 2, instructions: '...and quest'}, etc. ].
+    // This array should include the scheme_name not the scheme_id.
+
+    findSteps(id){
+        return db('schemes')
+        .join('steps', 'steps.scheme_id', 'schemes.id')
+        .select('steps.id','schemes.scheme_name', 'steps.step_number', 'steps.instructions')
+        .where({ "schemes.id" : id})
+        .orderBy('steps.step_number')
+    },
+
+// add(scheme):
+// Expects a scheme object.
+// Inserts scheme into the database.
+// Resolves to the newly inserted scheme, including id.
+    add(scheme){
+        return db('schemes').insert(scheme)
+        .then((id) => {
+            console.log(id)
+            return db('schemes')
+            .where({ id : id }).first();
+        })
+    },
+// update(changes, id):
+// Expects a changes object and an id.
+// Updates the scheme with the given id.
+// Resolves to the newly updated scheme object.
+    update(changes, id) {
+        return db('schemes')
+        .where({ id: id })
+        .update(changes)
+        .then(res => {
+            console.log(res)
+            return db('schemes')
+            .where({ id: id })
+        })
+    },
+
+// remove(id):
+// Removes the scheme object with the provided id.
+// Resolves to the removed scheme
+// Resolves to null on an invalid id.
+
+    remove(id) {
+        return db('schemes')
+        .where({ id : id })
+        .del()
+    }
+}
