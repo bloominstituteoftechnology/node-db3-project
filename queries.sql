f579888504593d19a20b6aae6aecb4bd552b77f6
Cora Krynick <krynickcora@gmail.com>
2020-11-04 21:55:08 -0600

    first push

--- queries.sql
+++ queries.sql
@@ -2,7 +2,22 @@
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
 
+select
+    p.productName as 'Product',
+    c.categoryName as 'Category'
+from Product as p
+join Category as c
+    on p.CategoryId = c.id;
+
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
+
+select
+    o.id as 'Order ID',
+    s.companyname as 'Shipper'
+from [Order] as o
+join Shipper as s
+    on o.shipvia =s.id
+where o.orderdate < '2012-08-09';
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
