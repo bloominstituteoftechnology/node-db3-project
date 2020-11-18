4edaf88265e2b8d8bbedda8c25d81c3c58a34b73
Samantha Goodman <antashma@gmail.com>
2020-11-17 20:01:39 -0500

    add base to scheme model

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,1 +1,45 @@
 // scheme-model
+const db = require('../data/db-config')
+
+module.exports = {
+    find,
+    findById,
+    findStep,
+    addStep,
+    add,
+    update,
+    remove
+}
+
+async function find() {
+
+}
+
+
+async function findById(id) {
+
+}
+
+async function findStep(id) {
+
+}
+
+
+async function addStep(id, step) {
+
+}
+
+
+async function add(scheme) {
+
+}
+
+
+async function update(id, changes) {
+
+}
+
+
+async function remove(id) {
+
+}
