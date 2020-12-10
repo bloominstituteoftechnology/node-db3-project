8febd916dbe0594e3e5dc4d81df3a650b96497e6
kristapants <peachykeen@gmail.com>
2020-12-09 23:08:42 -0600

    model completed up to add, which is returning and error someowhere around the .then. It's not getting the findbyid done right so, pick up from there.

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -7,15 +7,31 @@ const db = require("../../data/db-config.js");
     },
 
     findById(id) {
-
+        return db('schemes as s')
+        .where('s.id', id)
+        .first()
     },
 
     findSteps(id) {
+        return db('steps as st')
+        .join('schemes as sc', 'sc.id', 'st.scheme_id')
+        .select(
+            'st.id', 
+            'sc.scheme_name',
+            'step_number',
+            'instructions'
+        )
+        .where('st.scheme_id', id)
+        .orderBy('st.step_number', 'asc')
 
     },
 
     add(scheme) {
-
+        return db('schemes')
+        .insert(scheme)
+        .then(ids => {
+            return findById(ids[0]);
+        });
     },
 
     update(changes, id) {
