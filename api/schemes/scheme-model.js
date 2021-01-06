5d2e28e8517fd06d91c03ed637f17da6497d6326
Heather Beckman <heatherbeckman85@gmail.com>
2021-01-05 19:30:02 -0700

    started to writed all scheme functions

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -1,1 +1,25 @@
 // scheme-model
+
+function find() {
+
+}
+
+function findById(id) {
+
+}
+
+function findSteps(id) {
+
+}
+
+function add(scheme) {
+
+}
+
+function update(changes, id) {
+
+}
+
+function remove(id) {
+    
+}
