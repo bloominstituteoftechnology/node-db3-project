9346f3a5c5aa8fcf344842e997d438acb175ad6c
dwainejade <dwainejade@gmail.com>
2021-01-05 22:23:29 -0500

    welcome router working

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
-const server = require('./api/server.js');
+const express = require("express")
+const helmet = require("helmet")
+const welcomeRouter = require("./welcome/welcome-router")
+// const userRouter = require("./users/user-router")
 
+const server = require('./api/server.js');
 const PORT = process.env.PORT || 5000;
+
+server.use(helmet())
+server.use(express.json())
+server.use(welcomeRouter)
+// server.use(userRouter)
+
+server.use((err, req, res, next) => {
+  console.log(err)
+  res.status(500).json({
+    message: "Something went wrong",
+  })
+})
+
 
 server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
 });
+
+
+
+
+
+
