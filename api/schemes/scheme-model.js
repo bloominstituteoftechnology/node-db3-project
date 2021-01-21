92df0b87947e441283bad7be31552fe2ed81bb80
Ian Luchini <iancarloluchini@gmail.com>
2021-01-21 17:54:38 -0500

    created model for find, findSteps and add

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -1,1 +1,56 @@
 // scheme-model
+const db = require('../../data/db-config');
+
+const find = () => {
+    return db('schemes');
+}
+
+const findById = id => {
+    const scheme = db('schemes')
+        .where({ id })
+        .first();
+    if (!scheme)
+        return Promise.resolve(null);
+    else
+        return scheme;
+}
+
+const findSteps = id => {
+//     select 
+//     s.id, sch.scheme_name, s.step_number, s.instructions
+// from schemes sch
+// join steps s
+//     on sch.id = s.scheme_id
+// where sch.id = 5
+    return db('schemes as sch')
+        .join('steps as s', 'sch.id', 's.scheme_id')
+        .select('s.id', 'sch.scheme_name', 's.step_number', 's.instructions')
+        .where('sch.id', id);
+}
+
+const add = async scheme => {
+    try {
+        const newSchemeId = await db('schemes')
+            .insert(scheme);
+        return await findById(newSchemeId[0]);
+    } catch (error) {
+        return { error: 'Internal server error' };
+    }
+}
+
+const update = () => {
+    return db('schemes');
+}
+
+const remove = () => {
+    return db('schemes');
+}
+
+module.exports = {
+    find,
+    findById,
+    findSteps,
+    add,
+    update,
+    remove
+}
--- data/db-config.js
+++ data/db-config.js
@@ -1,1 +1,8 @@
 // db-config
+const knex = require('knex');
+const env = 'development';
+const configs = require('../knexfile'); 
+
+const db = knex(configs[env]);
+
+module.exports = db;
