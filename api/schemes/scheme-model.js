3d06916d87fea9a835cef5cb1942a296c8960705
Osvaldo Lievano <Osvaldoalievano@gmail.com>
2021-02-02 22:29:14 -0500

    find schemes and find schemes by ID complete

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -1,1 +1,36 @@
 // scheme-model
+
+const db = require('../../data/db-config');
+
+module.exports = {
+    find,
+    findById,
+    findStepsBySchemeId,
+    createScheme,
+    updateScheme,
+    deleteScheme
+}
+
+async function find(){
+    return await db('schemes');
+}
+
+async function findById(id){
+    return await db('schemes').where({ id })
+}
+
+async function findStepsBySchemeId(id){
+    return ('all wired up')
+}
+
+async function createScheme(data){
+    return ('all wired up')
+}
+
+async function updateScheme(id,changes){
+    return ('all wired up')
+}
+
+async function deleteScheme(id){
+    return ('all wired up')
+}
