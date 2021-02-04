21f75a1f65023faf3926d02feffcbdbe88096d48
jon del toro <61124054+jdeltoro88@users.noreply.github.com>
2021-02-03 20:25:38 -0700

    update db config, model js

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -1,1 +1,46 @@
 // scheme-model
+
+
+const db = require('../..data/db-config');
+
+async function find() {
+    return await db('schemes');
+
+
+}
+
+async function findById(id) {
+    return await db ('schemes').where({ id }).first();
+
+}
+
+async function findSteps(id) {
+    return await db.select(
+        'steps.id',
+        'schemes.scheme_name',
+        'steps.step_number',
+        'steps.instructions'
+    )
+    .where({ scheme_id: id })
+    .from('steps')
+    .join('schemes', 'steps.scheme_id', '=', 'schemes.id')
+    .orderBy('steps.step_number');
+}
+
+async function add(scheme) {
+    const [id] = await db('schemes').insert(scheme);
+    return { id, ...scheme };
+}
+
+async function update(changes, id) {
+    const count = await db('schemes').where({ id }).update(changes)
+    if (count === 0) throw new Error();
+    else return { id, ...changes };
+}
+
+async function remove(id) {
+    return await db('schemes').where({ id }).del();
+}
+
+
+module.exports = { find, findById, findsSteps, add, update, remove }
--- data/db-config.js
+++ data/db-config.js
@@ -1,1 +1,11 @@
 // db-config
+
+
+
+const knex = require('knex');
+
+const config = require('../knexfile.js')
+
+const db = knex(confug.development);
+
+module.exports = db;
