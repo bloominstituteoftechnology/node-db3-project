d99fc52dbd3663a71bf9e1c750b6b4294ad4cd02
Brian Choi <b.choi562@gmail.com>
2021-02-17 15:46:33 -0800

    mVP

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -1,1 +1,45 @@
 // scheme-model
+const db = require("../../data/db-config");
+
+const find = () => {
+    return db("schemes")
+}
+
+const findById = (id) => {
+    return db("schemes").where("id", id).first()
+}
+
+const findSteps = (id) => {
+    return db('steps')
+      .select('steps.id', 'schemes.scheme_name', 'steps.step_number', 'steps.instructions')
+      .join('schemes', 'schemes.id', 'steps.scheme_id')
+      .where('scheme_id', id)
+      .orderBy('steps.step_number')
+  }
+  
+  const add = (scheme) => {
+    return db('schemes').insert(scheme)
+  }
+  
+  const update = (changes, id) => {
+    return db('schemes')
+      .where('id', id)
+      .update(changes)
+      .then(id => {
+        return findById(id)
+      })
+  }
+  
+  const remove = (id) => {
+    return db('schemes').where({ id }).del()
+  }
+  
+  
+  module.exports = {
+    find,
+    findById,
+    findSteps,
+    add,
+    update,
+    remove
+  }
--- data/db-config.js
+++ data/db-config.js
@@ -1,1 +1,9 @@
 // db-config
+const knex = require("knex");
+const env = process.env.NODE_ENV || "development";
+const configs = require("../knexfile");
+
+const db = knex(configs[env]);
+
+module.exports = db;
+
