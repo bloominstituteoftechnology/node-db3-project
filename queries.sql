577223d3fa27b4a6d66aafbb7214d9642f55bc5f
Diego Roman <diegormnv209@gmail.com>
2021-02-17 14:29:04 -0800

    completed first two queries need to fix bug on third one

--- queries.sql
+++ queries.sql
@@ -2,8 +2,37 @@
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
 
+select
+    p.productName,
+    c.categoryName
+from
+    product as p
+join category as c
+on p.categoryid = c.id
+
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
+
+select
+    o.id,
+    s.companyname
+from 
+    [order] as o
+join shipper as s
+on o.shipvia = s.id
+where o.orderdate < "2012-08-09"
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
+select
+    p.productname,
+    sum(p.quantityperunit * p.unitsinstock) Quantity
+from 
+    orderdetail as od
+join product as p
+on od.unitprice = p.unitprice
+where od.orderid = 10251 
+order by p.productname
+
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+
+
