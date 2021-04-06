bcb68296591731d672f0254ff5bde03e445fb7b0
jon del toro <61124054+jdeltoro88@users.noreply.github.com>
2021-04-06 17:30:58 -0600

    middleware

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -1,38 +1,48 @@
+
+const Schemes = require('./scheme-model.js');
+
 /*
   If `scheme_id` does not exist in the database:
-
-  status 404
-  {
-    "message": "scheme with scheme_id <actual id> not found"
-  }
+  status 404{ "message": "scheme with scheme_id <actual id> not found" }
 */
 const checkSchemeId = (req, res, next) => {
+	// const { id } = req.params;
 
-}
+	// Schemes.findById(id)
+  //   .then((data) => {
+  //     if (!data) {
+  //       res.status(404).json({message: `Car with id ${id} is not found` })
+  //     } else {
+  //       next();
+  //     }
+  //   })
+	// 	.catch(next);
 
-/*
-  If `scheme_name` is missing, empty string or not a string:
+  next();
+};
 
-  status 400
-  {
-    "message": "invalid scheme_name"
-  }
-*/
 const validateScheme = (req, res, next) => {
+  const scheme = req.body;
 
+  if (!scheme.scheme_name || typeof scheme.scheme_name !== 'string') {
+    res.status(400).json({ message: "invalid scheme_name" });
+  } else {
+    next();
+  }
 }
 
-/*
-  If `instructions` is missing, empty string or not a string, or
-  if `step_number` is not a number or is smaller than one:
-
-  status 400
-  {
-    "message": "invalid step"
-  }
-*/
 const validateStep = (req, res, next) => {
+  const step = req.body;
 
+  if (
+		!step.instructions ||
+		typeof step.instructions !== 'string' ||
+		typeof step.step_number !== 'number' ||
+		step.step_number < 1) {
+      res.status(400).json({ message: "invalid step" })
+	} else {
+    next();
+  }
 }
 
 module.exports = {
