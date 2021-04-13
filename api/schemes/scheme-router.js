41b8f735453f3f1c264e3d04b823e8e41319c22b
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-04-13 14:31:25 +0200

    wip

--- api/schemes/scheme-router.js
+++ api/schemes/scheme-router.js
@@ -141,7 +141,7 @@ router.post('/:scheme_id/steps', checkSchemeId, validateStep, (req, res, next) =
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
-  res.status(500).json({
+  res.status(err.status || 500).json({
     sageAdvice: 'Finding the real error is 90% of the bug fix',
     error: err.message,
     stack: err.stack,
