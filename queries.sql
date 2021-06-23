e0fb11982ace5a3053c5d2a00133a748250160e0
artofmayhem <chef_tony_miller@yahoo.com>
2021-06-22 19:41:44 -1000

    TonyMiller

--- queries.sql
+++ queries.sql
@@ -7,3 +7,4 @@
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+
