35ae2f78e59f4deaea53758f3cb61f946475f30e
Stan <stan2dor@gmail.com>
2021-07-27 18:42:20 -0700

    project setup

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -1,3 +1,4 @@
+const Scheme = require("./scheme-model");
 /*
   If `scheme_id` does not exist in the database:
 
@@ -6,8 +7,19 @@
     "message": "scheme with scheme_id <actual id> not found"
   }
 */
-const checkSchemeId = (req, res, next) => {
 
+async function checkSchemeId(req, res, next) {
+  try {
+    const scheme_id = await Scheme.getById(req.params.id);
+    if (!scheme_id) {
+      next({ status: 404, message: "schema id does not exist" });
+    } else {
+      req.scheme_id = scheme_id;
+      next();
+    }
+  } catch (err) {
+    res.status(500).json({ message: "problem finding scheme" });
+  }
 }
 
 /*
@@ -18,9 +30,7 @@ const checkSchemeId = (req, res, next) => {
     "message": "invalid scheme_name"
   }
 */
-const validateScheme = (req, res, next) => {
-
-}
+const validateScheme = (req, res, next) => {};
 
 /*
   If `instructions` is missing, empty string or not a string, or
@@ -31,12 +41,10 @@ const validateScheme = (req, res, next) => {
     "message": "invalid step"
   }
 */
-const validateStep = (req, res, next) => {
-
-}
+const validateStep = (req, res, next) => {};
 
 module.exports = {
   checkSchemeId,
   validateScheme,
   validateStep,
-}
+};
