06389929775a5a0abc6e0f03d14e40b37bbc4031
Joey Bertschler <joey.bertschler@gmail.com>
2021-09-22 16:42:35 +0200

    more middleware

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -33,8 +33,17 @@ const checkSchemeId = async (req, res, next) => {
   }
 */
 const validateScheme = (req, res, next) => {
-
+  const { scheme_name } = req.body
+  if (scheme_name === undefined ||
+      typeof scheme_name !== 'string' ||
+      !scheme_name.trim()
+  ) { 
+    next({status: 400, message: "invalid scheme_name"})
+  } else {
+    next()
+  }
 }
+
 
 /*
   If `instructions` is missing, empty string or not a string, or
