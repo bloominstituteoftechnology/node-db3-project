eb0518478aa357a018ad1000b236d7a1cb8fdd9b
Stan <stan2dor@gmail.com>
2021-09-21 18:23:55 -0700

    Project & Server Setup

--- api/server.js
+++ api/server.js
@@ -1,10 +1,19 @@
-const express = require('express');
+const express = require("express");
+const helmet = require("helmet");
 
-const SchemeRouter = require('./schemes/scheme-router.js');
+const SchemeRouter = require("./schemes/scheme-router.js");
 
 const server = express();
 
+server.use(helmet());
 server.use(express.json());
-server.use('/api/schemes', SchemeRouter);
+
+server.use("/api/schemes", SchemeRouter);
+
+// eslint-disable-next-line
+server.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).json({ message: "Something went wrong" });
+});
 
 module.exports = server;
