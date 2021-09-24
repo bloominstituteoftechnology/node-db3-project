9bab975f05a165ab1677aa12869a54ea3c985b1b
Joey Bertschler <joey.bertschler@gmail.com>
2021-09-24 18:05:48 +0200

    scheme model works, combined tables, then selected, changed names, added count with groupby similar to the python function

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -1,3 +1,5 @@
+const db = require('../../data/db-config')
+
 function find() { // EXERCISE A
   /*
     1A- Study the SQL query below running it in SQLite Studio against `data/schemes.db3`.
@@ -15,6 +17,11 @@ function find() { // EXERCISE A
     2A- When you have a grasp on the query go ahead and build it in Knex.
     Return from this function the resulting dataset.
   */
+ return db('schemes as sc')
+  .leftJoin('steps as st', 'sc.scheme_id', 'st.scheme_id')
+  .select('sc.*')
+  .count('st.step_id as number_of_steps')
+  .groupBy('sc.scheme_id')
 }
 
 function findById(scheme_id) { // EXERCISE B
