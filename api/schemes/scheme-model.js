3b4f16117a164104375c06eacf88d7948d5b53e7
Gabe Chilson <g.chilson34@gmail.com>
2021-10-19 20:25:46 -0600

    finished MVP tasks

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -1,8 +1,10 @@
+const { fromPairs } = require('lodash')
+const db = require('../../data/db-config')
+
 function find() { // EXERCISE A
   /*
     1A- Study the SQL query below running it in SQLite Studio against `data/schemes.db3`.
     What happens if we change from a LEFT join to an INNER join?
-
       SELECT
           sc.*,
           count(st.step_id) as number_of_steps
@@ -11,16 +13,22 @@ function find() { // EXERCISE A
           ON sc.scheme_id = st.scheme_id
       GROUP BY sc.scheme_id
       ORDER BY sc.scheme_id ASC;
-
     2A- When you have a grasp on the query go ahead and build it in Knex.
     Return from this function the resulting dataset.
   */
+
+    db('schemes').as('sc')
+      .count("st.step_id").as("number_of_steps")
+      .leftJoin('step').as('st')
+        .on("sc.scheme_id", "=", "st.scheme_id")
+      .groupBy("sc.scheme_id")
+      .orderBy("sc.scheme_id", "desc")
+
 }
 
 function findById(scheme_id) { // EXERCISE B
   /*
     1B- Study the SQL query below running it in SQLite Studio against `data/schemes.db3`:
-
       SELECT
           sc.scheme_name,
           st.*
@@ -29,13 +37,16 @@ function findById(scheme_id) { // EXERCISE B
           ON sc.scheme_id = st.scheme_id
       WHERE sc.scheme_id = 1
       ORDER BY st.step_number ASC;
-
     2B- When you have a grasp on the query go ahead and build it in Knex
     making it parametric: instead of a literal `1` you should use `scheme_id`.
-
+    */
+    db('schemes as sc')
+    .joinLeft('steps as st', 'sc.scheme_id', '=', 'st.scheme_id')
+    .where('sc.scheme_id', '=', 'scheme_id')
+    .orderBy('st.step_number', 'asc')
+    /*
     3B- Test in Postman and see that the resulting data does not look like a scheme,
     but more like an array of steps each including scheme information:
-
       [
         {
           "scheme_id": 1,
@@ -53,10 +64,8 @@ function findById(scheme_id) { // EXERCISE B
         },
         // etc
       ]
-
     4B- Using the array obtained and vanilla JavaScript, create an object with
     the structure below, for the case _when steps exist_ for a given `scheme_id`:
-
       {
         "scheme_id": 1,
         "scheme_name": "World Domination",
@@ -74,9 +83,7 @@ function findById(scheme_id) { // EXERCISE B
           // etc
         ]
       }
-
     5B- This is what the result should look like _if there are no steps_ for a `scheme_id`:
-
       {
         "scheme_id": 7,
         "scheme_name": "Have Fun!",
@@ -90,7 +97,6 @@ function findSteps(scheme_id) { // EXERCISE C
     1C- Build a query in Knex that returns the following data.
     The steps should be sorted by step_number, and the array
     should be empty if there are no steps for the scheme:
-
       [
         {
           "step_id": 5,
@@ -106,12 +112,19 @@ function findSteps(scheme_id) { // EXERCISE C
         }
       ]
   */
+
+    db('steps as st')
+      .joinRight('scheme as sc', 'step_id', '=', 'sc.step_id')
+      .select('step_id', 'step_number', 'instructions', 'scheme_name')
+      .orderBy('st.step_number', 'desc')
 }
 
 function add(scheme) { // EXERCISE D
   /*
     1D- This function creates a new scheme and resolves to _the newly created scheme_.
   */
+  db('scheme as sc')
+  .insert({shceme_name: scheme})
 }
 
 function addStep(scheme_id, step) { // EXERCISE E
@@ -120,6 +133,8 @@ function addStep(scheme_id, step) { // EXERCISE E
     and resolves to _all the steps_ belonging to the given `scheme_id`,
     including the newly created one.
   */
+ db('step as st')
+  .insert([{instructions: step}, {scheme_id: scheme_id}])
 }
 
 module.exports = {
