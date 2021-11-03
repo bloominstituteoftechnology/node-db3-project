d63237ff9a3afdc60f66c19e893b22a462ae1cd4
Raj-04 <1204patel.raj@gmail.com>
2021-11-03 18:22:42 -0400

    CheckSchemeId middleware is created

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -1,3 +1,5 @@
+const Schemes = require('./scheme-model')
+
 /*
   If `scheme_id` does not exist in the database:
 
@@ -6,8 +8,21 @@
     "message": "scheme with scheme_id <actual id> not found"
   }
 */
-const checkSchemeId = (req, res, next) => {
-
+const checkSchemeId = async (req, res, next) => {
+  try {
+    const scheme_id = req.params.scheme_id
+    const scheme = await Schemes.findById(scheme_id)
+    if(scheme) {
+      next()
+    } else {
+      next({
+        status: 404,
+        message: `scheme with scheme_id ${scheme_id} not found`
+      })
+    }
+  } catch (err) {
+    next(err)
+  }
 }
 
 /*
