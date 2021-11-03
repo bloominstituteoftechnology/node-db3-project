eaed68a337b3481f4d3ddcd00c5f937f09ad950f
Raj-04 <1204patel.raj@gmail.com>
2021-11-03 18:35:24 -0400

    validateStep middleware is done

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -33,17 +33,22 @@ const validateScheme = (req, res, next) => {
   }
 }
 
-/*
-  If `instructions` is missing, empty string or not a string, or
-  if `step_number` is not a number or is smaller than one:
-
-  status 400
-  {
-    "message": "invalid step"
-  }
-*/
 const validateStep = (req, res, next) => {
-
+  const { instructions, step_number } = req.body
+  if (
+    instructions === undefined ||
+    typeof instructions !== 'string' ||
+    !instructions.trim() ||
+    typeof step_number !== 'number' ||
+    step_number < 1
+  ) {
+    next({
+      status:400,
+      message: 'invalid step'
+    })
+  } else {
+    next()
+  }
 }
 
 module.exports = {
