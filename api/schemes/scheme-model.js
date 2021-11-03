58e73a718acc16ae407e599051c29e4360051b8d
Raj-04 <1204patel.raj@gmail.com>
2021-11-03 19:24:17 -0400

    Function findSteps model working

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -35,27 +35,12 @@ async function findById(scheme_id) {
   return result
 }
 
-function findSteps(scheme_id) { // EXERCISE C
-  /*
-    1C- Build a query in Knex that returns the following data.
-    The steps should be sorted by step_number, and the array
-    should be empty if there are no steps for the scheme:
-
-      [
-        {
-          "step_id": 5,
-          "step_number": 1,
-          "instructions": "collect all the sheep in Scotland",
-          "scheme_name": "Get Rich Quick"
-        },
-        {
-          "step_id": 4,
-          "step_number": 2,
-          "instructions": "profit",
-          "scheme_name": "Get Rich Quick"
-        }
-      ]
-  */
+function findSteps(scheme_id) {
+  return db("steps as st")
+    .leftJoin("schemes as sc", "sc.scheme_id", "st.scheme_id")
+    .where("sc.scheme_id", scheme_id)
+    .select("sc.scheme_name", "st.*", "sc.scheme_id")
+    .orderBy("st.step_number");
 }
 
 function add(scheme) { // EXERCISE D
