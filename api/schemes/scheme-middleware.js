2dc8d7806f8f9f3fba8875ca0768c2cb6908f695
Emperor D. Pengin <emperord.pengin@gmail.com>
2021-11-04 09:07:20 -0400

    mvp

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -1,3 +1,5 @@
+const Schemes = require('./scheme-model');
+const yup = require('yup');
 /*
   If `scheme_id` does not exist in the database:
 
@@ -7,8 +9,31 @@
   }
 */
 const checkSchemeId = (req, res, next) => {
-
+  
+  Schemes.findById(req.params.scheme_id)
+    .then( schemeById => {
+      if (schemeById) {
+        req.scheme = schemeById;
+        next();
+      } else {
+        next({status: 404, message: `scheme with scheme_id <${req.params.scheme_id}> not found`})
+      }
+    })
+    .catch(next)
 }
+
+
+
+
+const schemeValidation = yup.object().shape({
+  scheme_name: yup
+    .string()
+    .trim()
+    .typeError("invalid scheme_name")
+    .required("invalid scheme_name")
+})
+
+
 
 /*
   If `scheme_name` is missing, empty string or not a string:
@@ -19,8 +44,39 @@ const checkSchemeId = (req, res, next) => {
   }
 */
 const validateScheme = (req, res, next) => {
-
+  schemeValidation.validate(req.body,
+    {
+      strict: true,
+      stripUnknown: true
+    })
+      .then(validate => {
+        Schemes.find()
+          .then( schemes => {
+            let foundSchemes = schemes.filter(scheme => scheme.scheme_name === validate.scheme_name)
+            if (foundSchemes.length < 1) {
+              req.body = validate;
+              next();
+            } else {
+              next({ status: 400, message: 'scheme name is taken'})
+            }
+          })
+          .catch(next)
+      })
+      .catch(err => {
+        next({ status: 400, message: err.message})
+      })
 }
+
+const stepValidation = yup.object().shape({
+  instructions: yup
+    .string()
+    .trim()
+    .typeError("invalid scheme_name")
+    .required("invalid scheme_name"),
+  step_number: yup
+    .number()
+    .min(1, 'invalid step')
+})
 
 /*
   If `instructions` is missing, empty string or not a string, or
@@ -32,7 +88,19 @@ const validateScheme = (req, res, next) => {
   }
 */
 const validateStep = (req, res, next) => {
-
+  
+  stepValidation.validate(req.body,
+    {
+      strict: true,
+      stripUnknown: true
+    })
+      .then(validate => {
+        req.body =validate;
+        next()
+      })
+      .catch(err => {
+        next({ status: 400, message: err.message})
+      })
 }
 
 module.exports = {
