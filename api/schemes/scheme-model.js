6dd4bc93b1d63e383a3fce7e444c376dfce42eb7
Alieze-Ali <71855821+Alieze-Ali@users.noreply.github.com>
2021-12-21 22:52:05 -0800

    first get api router working

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -1,4 +1,6 @@
-function find() { // EXERCISE A
+  const db = require('../../data/db-config')
+  
+  function find() { // EXERCISE A
   /*
     1A- Study the SQL query below running it in SQLite Studio against `data/schemes.db3`.
     What happens if we change from a LEFT join to an INNER join?
@@ -15,6 +17,11 @@ function find() { // EXERCISE A
     2A- When you have a grasp on the query go ahead and build it in Knex.
     Return from this function the resulting dataset.
   */
+ return db('schemes as sc')
+ .leftJoin('steps as st', 'sc.scheme_id', 'st.scheme_id')
+ .select('sc.*')
+ .count('st.step_id as number_of_steps')
+ .groupBy('sc.scheme_id')
 }
 
 function findById(scheme_id) { // EXERCISE B
