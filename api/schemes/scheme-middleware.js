3b3827cae6e7b24e3b9bd754438591b73bcafd7e
Alieze-Ali <71855821+Alieze-Ali@users.noreply.github.com>
2021-12-21 22:14:37 -0800

    a few tests passing

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -19,6 +19,8 @@ const checkSchemeId = async(req, res, next) => {
         status: 404,
         message: `scheme with scheme_id ${req.params.scheme_id} not found`,
       })
+    }else{
+      next()
     }       
   } catch(err) {
     next(err)
@@ -34,7 +36,19 @@ const checkSchemeId = async(req, res, next) => {
   }
 */
 const validateScheme = (req, res, next) => {
-
+ const {shceme_name} = req.body
+ if(
+   shceme_name === undefined ||
+   typeof shceme_name !== 'string' ||
+   !shceme_name.trim()
+ ) {
+   next({ 
+    status: 400,
+    message: 'invalid scheme_name'
+    })
+ } else {
+   next()
+ }
 }
 
 /*
@@ -47,7 +61,19 @@ const validateScheme = (req, res, next) => {
   }
 */
 const validateStep = (req, res, next) => {
-
+  const{ instructions, step_number } = req.body
+  if (
+    instructions === undefined ||
+    typeof instructions !== 'string' ||
+    !instructions.trim() ||
+    typeof step_number !== 'number' ||
+    step_number < 1
+  ) {
+    const error = { status: 400, message: 'invalid step' }
+    next(error)
+  }else{
+    next()
+  }
 }
 
 module.exports = {
