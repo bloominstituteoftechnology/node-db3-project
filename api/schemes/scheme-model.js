ab052cafd86f259d7ec26872c406fbf8544de152
Alieze-Ali <71855821+Alieze-Ali@users.noreply.github.com>
2021-12-24 18:47:08 -0800

    second post working

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -159,6 +159,11 @@ function add(scheme) { // EXERCISE D
   /*
     1D- This function creates a new scheme and resolves to _the newly created scheme_.
   */
+  return db('schemes').insert(scheme)
+  .then(([scheme_id])=>{
+    return db('schemes').where('scheme_id', scheme_id).first()
+  
+  })
 }
 
 function addStep(scheme_id, step) { // EXERCISE E
@@ -167,6 +172,17 @@ function addStep(scheme_id, step) { // EXERCISE E
     and resolves to _all the steps_ belonging to the given `scheme_id`,
     including the newly created one.
   */
+ return db('steps').insert({
+   ...step,
+   scheme_id
+ })
+ .then(()=>{
+  return db('steps as st')
+  .join('schemes as sc', 'sc.scheme_id', 'st.scheme_id')
+  .select('step_id', 'step_number', 'instructions', 'scheme_name')
+  .orderBy('step_number')
+  .where('sc.scheme_id', scheme_id)
+ })
 }
 
 module.exports = {
