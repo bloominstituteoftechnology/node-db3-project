922a977980484f7f45ee655d19e2cf50e0c93120
Alieze-Ali <71855821+Alieze-Ali@users.noreply.github.com>
2021-12-24 17:09:29 -0800

    first 3 get requests working

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -115,8 +115,17 @@ async function findById(scheme_id) { // EXERCISE B
 
     }
 
-function findSteps(scheme_id) { // EXERCISE C
+async function findSteps(scheme_id) { // EXERCISE C
   /*
+
+  select
+  step_id, step_number, instructions, scheme_name
+  from schemes as sc
+  left join steps as st
+  on sc.scheme_id = st.scheme_id
+  where sc.scheme_id = 1
+  order by step_number;
+
     1C- Build a query in Knex that returns the following data.
     The steps should be sorted by step_number, and the array
     should be empty if there are no steps for the scheme:
@@ -136,6 +145,14 @@ function findSteps(scheme_id) { // EXERCISE C
         }
       ]
   */
+ const rows = await db('schemes as sc')
+      .leftJoin('steps as st', 'sc.scheme_id', 'st.scheme_id')
+      .select('st.step_id', 'st.step_number', 'instructions', 'sc.scheme_name')
+      .where('sc.scheme_id', scheme_id)
+      .orderBy('step_number')
+
+      if (!rows[0].step_id) return []
+      return rows
 }
 
 function add(scheme) { // EXERCISE D
