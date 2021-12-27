dbbe4b796f70a87a1d066b9447182c42d8ef9640
tamara-taylor <tamaralynntaylor@yahoo.com>
2021-12-27 18:32:04 -0500

    3 not passing

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -1,59 +1,72 @@
 const db = require('../../data/db-config')
+const Schemes = require('./scheme-model')
+const yup = require('yup')
 
-/*
-  If `scheme_id` does not exist in the database:
-
-  status 404
-  {
-    "message": "scheme with scheme_id <actual id> not found"
-  }
-*/
 const checkSchemeId = async (req, res, next) => {
   try{
-    const check = await db('schemes')
-    .where('scheme_id', req.params.scheme_id)
+    const scheme_id = req.params.scheme_id
+    const scheme = await db('schemes')
+    .where('scheme_id', scheme_id)
     .first()
-    if(!check){
-      res.status(404).json({message: `scheme with scheme_id ${req.params.scheme_id} not found`})
-    }else{
+    if(scheme){
       next()
+    }else{
+      next({status: 404, message: `scheme with scheme_id ${scheme_id} not found`})
     }
-  }catch(err){
+  }
+  catch(err){
     next(err)
   }
 }
 
-/*
-  If `scheme_name` is missing, empty string or not a string:
+const schemeValidation = yup.object().shape({
+  scheme_name: yup
+  .string()
+  .trim()
+  .typeError('invalid scheme_name')
+  .required('invalid scheme_name')
+})
 
-  status 400
-  {
-    "message": "invalid scheme_name"
-  }
-*/
 const validateScheme = (req, res, next) => {
-  const {scheme_name} = req.body
-  if(scheme_name === undefined || typeof scheme_name !== 'string' || !scheme_name.trim()){
-    res.status(400).json({message: 'invalid scheme_name'})
-  }else{
-    next()
-  }
+  schemeValidation.validate(req.body, {strict: true, stripUnknown: true})
+  .then(validate => {
+    Schemes.find()
+    .then(schemes => {
+      let foundSchemes = schemes.filter(scheme => scheme.scheme_name === validate.scheme_name)
+      if(foundSchemes.length < 1){
+        req.body = validate
+        next()
+      }else{
+        next({status: 400, message: 'invalid scheme_name'})
+      }
+    })
+    .catch(next)
+  })
+  .catch(err => {
+    next({status: 400, message: err.message})
+  })
 }
 
-/*
-  If `instructions` is missing, empty string or not a string, or
-  if `step_number` is not a number or is smaller than one:
+const stepValidation = yup.object().shape({
+  instructions: yup
+  .string()
+  .trim()
+  .typeError('invalid scheme_name')
+  .required('invalid scheme_name'),
+  step_number: yup
+  .number()
+  .min(1, 'invalid step')
+})
 
-  status 400
-  {
-    "message": "invalid step"
-  }
-*/
 const validateStep = (req, res, next) => {
-  const {instructions, step_number} = req.body
-  if(instructions === undefined || typeof instructions !== 'string' || !instructions.trim() || typeof step_number !== 'number' || step_number < 1){
-    res.status(400).json({message: 'invalid step'})
-  }
+  stepValidation.validate(req.body, {strict: true, stripUnknown: true})
+  .then(validate => {
+    req.body = validate
+    next()
+  })
+  .catch(err => {
+    next({status: 400, message: 'invalid step'})
+  })
 }
 
 module.exports = {
--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -1,13 +1,12 @@
 const db = require('../../data/db-config')
 
-async function find(){
- const rows = await db('schemes as sc')
+function find(){
+ return db('schemes as sc')
  .select('sc.*')
  .leftJoin('steps as st', 'sc.scheme_id', 'st.scheme_id')
- .groupBy('sc.scheme_id')
- .orderBy('sc.scheme_id')
  .count('st.step_id as number_of_steps')
- return rows
+ .groupBy('sc.scheme_id')
+ .orderBy('sc.scheme_id', 'asc')
 }
 
 async function findById(scheme_id){ 
@@ -15,34 +14,37 @@ async function findById(scheme_id){
  .leftJoin('steps as st', 'sc.scheme_id', 'st.scheme_id')
  .where('sc.scheme_id', scheme_id)
  .select('st.*', 'sc.scheme_name', 'sc.scheme_id')
- .orderBy('st.step_number')
-
- if(!rows[0]){
-   return null
- }else{
-   return{
-     scheme_id: rows[0].scheme_id,
-     scheme_name: rows[0].scheme_name,
-     steps: rows[0].step_id
-     ? rows.map((row) => {
-       return{
-         step_id: row.step_id,
-         step_number: row.step_number,
-         instructions: row.instructions
-       }
-     })
-     :[]
-   }
- }
+ .orderBy('st.step_number', 'asc')
+ 
+ const result = {
+   scheme_id: rows[0].scheme_id,
+   scheme_name: rows[0].scheme_name,
+   steps: []
+  }
+  
+  rows.forEach(row => {
+    if(row.step_id) {
+      result.steps.push({
+        step_id: row.step_id,
+        step_number: row.step_number,
+        instructions: row.instructions,
+      })
+    }
+  })
+  return result
 }
 
 async function findSteps(scheme_id){
  const rows = await db('schemes as sc')
- .join('steps as st', 'sc.scheme_id', 'st.scheme_id')
+ .leftJoin('steps as st', 'sc.scheme_id', 'st.scheme_id')
  .select('st.step_id', 'st.step.number', 'instructions', 'sc.scheme_name')
  .where('sc.scheme_id', scheme_id)
- .orderBy('step_number')
- return rows
+ .orderBy('st.step_number')
+ if(!rows[0].step_id){
+   return []
+ }else{
+   return rows
+ }
 }
 
 async function add(scheme){
@@ -58,7 +60,7 @@ function addStep(scheme_id, step){
    return db('steps as st')
    .join('schemes as sc', 'sc.scheme_id', 'st.scheme_id')
    .select('step_id', 'step_number', 'instructions', 'scheme_name')
-   .orderBy('step_number')
+   .orderBy('step_number', 'asc')
    .where('sc.scheme_id', scheme_id)
  })
 }
