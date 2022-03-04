465d31f24f35b39e509a29ab373a0842e528e6af
Alieze-Ali <71855821+Alieze-Ali@users.noreply.github.com>
2022-03-04 00:15:20 -0800

    first post api working

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -151,6 +151,10 @@ function add(scheme) { // EXERCISE D
   /*
     1D- This function creates a new scheme and resolves to _the newly created scheme_.
   */
+ return db('schemes').insert(scheme)
+ .then(([scheme_id]) => {
+   return db('schemes').where('scheme_id', scheme_id).first()
+ })
 }
 
 function addStep(scheme_id, step) { // EXERCISE E
