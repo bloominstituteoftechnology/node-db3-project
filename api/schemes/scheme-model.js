381dc92012100ff85380986d2f96394fa1ca7149
Alieze-Ali <71855821+Alieze-Ali@users.noreply.github.com>
2022-03-03 23:54:48 -0800

    first 3 get apis working

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -116,7 +116,7 @@ async function findById(scheme_id) { // EXERCISE B
       return result
 }
 
-function findSteps(scheme_id) { // EXERCISE C
+async function findSteps(scheme_id) { // EXERCISE C
   /*
     1C- Build a query in Knex that returns the following data.
     The steps should be sorted by step_number, and the array
@@ -137,6 +137,14 @@ function findSteps(scheme_id) { // EXERCISE C
         }
       ]
   */
+ const rows = await db('schemes as sc')
+      .leftJoin('steps as st', 'sc.scheme_id', 'st.scheme_id')
+      .select('st.step_id', 'st.step_number', 'instructions', 'sc.scheme_name')
+      .where('sc.scheme_id', scheme_id)
+      .orderBy('step_number')
+
+      if (!rows[0].step_id) return []
+      return rows
 }
 
 function add(scheme) { // EXERCISE D
