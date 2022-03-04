e42cfe6da69043fdb8cd215d1a3ad9957796801a
Alieze-Ali <71855821+Alieze-Ali@users.noreply.github.com>
2022-03-03 23:29:25 -0800

    first two apis working

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -25,7 +25,7 @@ function find() { // EXERCISE A
  .groupBy('sc.scheme_id')
 }
 
-function findById(scheme_id) { // EXERCISE B
+async function findById(scheme_id) { // EXERCISE B
   /*
     1B- Study the SQL query below running it in SQLite Studio against `data/schemes.db3`:
 
@@ -91,6 +91,29 @@ function findById(scheme_id) { // EXERCISE B
         "steps": []
       }
   */
+      const rows = await db('schemes as sc')
+      .leftJoin('steps as st', 'sc.scheme_id', 'st.scheme_id')
+      .where('sc.scheme_id', scheme_id)
+      .select('st.*', 'sc.scheme_name', 'sc.scheme_id')
+      .orderBy('st.step_number')
+
+      const result = {
+        scheme_id: rows[0].scheme_id,
+        scheme_name: rows[0].scheme_name,
+        steps: []
+      }
+     
+      rows.forEach(row => {
+        if (row.step_id) {
+          result.steps.push({
+            step_id: row.step_id,
+            step_number: row.step_number,
+            instructions: row.instructions,
+          })
+        }
+      })
+
+      return result
 }
 
 function findSteps(scheme_id) { // EXERCISE C
